8edc8db136edf9c1d46fcae1a319938ff896da8f
B18150219李飞 <www.1259846094@qq.com>
2020-07-10 12:52:30 +0800

    spring第七天学习(注解，xml配置事物，事物的传播行为，隔离级别，超时操作，只读，回滚)

--- src/lesson06/book_xml_Test.java
+++ src/lesson06/book_xml_Test.java
@@ -1,13 +1,14 @@
 package lesson06;
 
-import lesson06.Book.Controller.BookController;
+import lesson06.book_xml.Controller.BookController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class book_Test {
+public class book_xml_Test {
     public static void main(String[] args) {
-        ApplicationContext ac=new ClassPathXmlApplicationContext("/conf4/book.xml");
+        ApplicationContext ac=new ClassPathXmlApplicationContext("/conf4/book_xml.xml");
         BookController bookController = ac.getBean("bookController", BookController.class);
-        bookController.buyBook();
+//        bookController.buyBook();
+        bookController.checkOut();
     }
 }
